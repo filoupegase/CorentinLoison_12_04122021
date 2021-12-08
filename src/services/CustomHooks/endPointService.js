b75277fbd770e569cdc9ab8ae29e461454e759d8
filoupegase <loisoncorentin@yahoo.fr>
2021-12-08 22:23:01 +0100

    delete for branch

--- src/services/CustomHooks/endPointService.js
+++ src/services/CustomHooks/endPointService.js
@@ -4,21 +4,9 @@
  * @returns {string|null} match endpoint associated to the service and the user id
  */
 export function endPointService(service, userId) {
-  const ACTIVITIES_URL = `user/${ userId }/performance`;
-  const AVERAGE_SESSIONS_URL = `user/${ userId }/average-sessions`;
-  const DAILY_ACTIVITY_URL = `user/${ userId }/activity`;
   const USER_URL = `${ userId }`;
 
   switch (service) {
-    case 'activities':
-      return ACTIVITIES_URL;
-
-    case 'average-sessions':
-      return AVERAGE_SESSIONS_URL;
-
-    case 'daily-activity':
-      return DAILY_ACTIVITY_URL;
-
     case 'firstName':
       return USER_URL;
 
